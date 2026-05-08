e8deb657e11d786a48ef108d35836eec7c271e60
kbanning04 <kbanning04@gmail.com>
2026-05-08 14:03:15 +0100

    Inital commit

---
+++ days_api/app.py
@@ -0,0 +1,39 @@
+"""This file defines the API routes."""
+
+# pylint: disable = no-name-in-module
+
+from datetime import datetime, date
+
+from flask import Flask, Response, request, jsonify
+
+from date_functions import (convert_to_datetime, get_day_of_week_on,
+                            get_days_between, get_current_age)
+
+app_history = []
+
+app = Flask(__name__)
+
+
+def add_to_history(current_request):
+    """Adds a route to the app history."""
+    app_history.append({
+        "method": current_request.method,
+        "at": datetime.now().strftime("%d/%m/%Y %H:%M"),
+        "route": current_request.endpoint
+    })
+
+def clear_history():
+    """Clears the app history."""
+    app_history.clear()
+
+
+@app.get("/")
+def index():
+    """Returns an API welcome messsage."""
+    return jsonify({"message": "Welcome to the Days API."})
+
+
+if __name__ == "__main__":
+    app.config['TESTING'] = True
+    app.config['DEBUG'] = True
+    app.run(port=8080, debug=True)
